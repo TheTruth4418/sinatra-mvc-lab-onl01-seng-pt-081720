3722eebc8e3723cc1f220e33c868957dbf7a3708
William West <wwest466@gmail.com>
2020-11-18 18:45:44 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,7 +1,12 @@
 class PigLatinizer
 
   def pigLatinize(string)
+    a = string.split(" ")
 
+    a.each do |word|
+      word.piglatinize_word
+    end
+    a.join(" ")
   end
 
   def piglatinize_word(word)
