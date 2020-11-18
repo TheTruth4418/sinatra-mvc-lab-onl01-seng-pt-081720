bcc5a58daff2ff3f5f721277f296bdaeafdcaeb8
William West <wwest466@gmail.com>
2020-11-18 18:50:42 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,26 +1,37 @@
 class PigLatinizer
 
-  def pigLatinize(string)
-    a = string.split(" ")
-    a.map {|word| piglatize_word(word)}
-    a.join(" ")
-  end
 
-  def piglatinize_word(word)
-    first_letter = word[0].downcase
-    if first_letter == "a" || first_letter == "e" || first_letter == "i" || first_letter == "o" || first_letter == "u"
-        "#{word}way"
-    else
-      consonants = []
-    consonants << word[0]
-      if ["a", "e", "i", "o", "u"].include?(word[1]) == false
-        consonants << word[1]
-        if ["a", "e", "i", "o", "u"].include?(word[2]) == false
-          consonants << word[2]
-        end
-      end
-    "#{word[consonants.length..-1] + cons
-    end
-  end
-
+def piglatinize(thing)
+  pig(thing)
 end
+
+def to_pig_latin(string)
+  pig(string)
+end
+
+def pig(thing)
+  alpha = ('a'..'z').to_a + ('A'..'Z').to_a
+  vowels = %w[a e i o u] + %w[A E I O U]
+  consonants = alpha - vowels
+
+  showme = []
+    input = thing.split(' ') 
+      input.each do |word|
+          if word.length < 2
+             showme << word + "way" 
+          elsif vowels.include?(word[0])
+             showme <<  word + "way"
+          elsif consonants.include?(word[0]) && consonants.include?(word[1]) && consonants.include?(word[2])
+             showme <<  word[3..-1] + word[0..2] + 'ay'
+          elsif consonants.include?(word[0]) && consonants.include?(word[1])
+             showme <<  word[2..-1] + word[0..1] + 'ay'
+          elsif consonants.include?(word[0])
+             showme <<  word[1..-1] + word[0] + 'ay'
+          else 
+             showme <<  word + '?'
+          end
+      end 
+  showme.join(', ').gsub(/,/, '')
+end
+
+end 
