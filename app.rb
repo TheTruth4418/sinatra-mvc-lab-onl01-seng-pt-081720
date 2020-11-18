ce385052d6312586fdf8080e56d0047db64e7038
William West <wwest466@gmail.com>
2020-11-18 19:05:37 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,7 +5,7 @@ class App < Sinatra::Base
     erb :user_input
   end
 
-  post '/' do
+  post '/piglatinize' do
     mod = Piglatinizer.new
     @string = mod.pig(params["user_phrase"])
     erb :piglatinize
